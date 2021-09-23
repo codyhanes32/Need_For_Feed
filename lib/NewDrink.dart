4a3c230b77b90b55c439889fa72f01086e52f414
Cody Hanes <codyhanes@Roberts-iMac.attlocal.net>
2021-09-22 21:17:40 -0700

    added beer catagory to new drink page

--- lib/NewDrink.dart
+++ lib/NewDrink.dart
@@ -6,10 +6,12 @@ class NewDrink extends StatefulWidget {
   @override
   _NewDrinkState createState() => _NewDrinkState();
 }
-
 class _NewDrinkState extends State<NewDrink> {
   double rating = 3;
-  String beerType = 'Ale';
+  List<String> options = <String>['Ale', 'Lager', 'Porter', 'Stout', 'Blonde Ale', 'Brown Ale',
+    'Pale Ale', 'Indian Pale Ale (IPA)', 'Wheat', 'Pilsner', 'Sour'];
+  String dropdownValue = 'Ale';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +27,15 @@ class _NewDrinkState extends State<NewDrink> {
           children: [
 
             Container(
+              child: Text('New Beer',
+              style: TextStyle(
+                fontFamily: 'Hind',
+                color: Colors.blueAccent,
+                fontSize: 70,
+              )),
+            ),
+
+            Container(
               margin: EdgeInsets.all(30),
               child: TextField(
                 decoration: const InputDecoration(
@@ -35,10 +46,52 @@ class _NewDrinkState extends State<NewDrink> {
               ),
             ),
 
-
+            Container(
+              margin: new EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Text('Select Beer Type',
+                  style: TextStyle(
+                    fontFamily: 'Hind',
+                    color: Colors.black,
+                    height: 1,
+                    fontSize: 12,
+                    fontStyle: FontStyle.italic,
+                  )),
+                  Container(
+                      margin: new EdgeInsets.symmetric(horizontal: 30.0, vertical: 50),
+                      height: 30,
+                      width: 180,
+                      child: DropdownButton<String>(
+                      value: dropdownValue,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                        },
+                      style: const TextStyle(color: Colors.blue),
+                      selectedItemBuilder: (BuildContext context) {
+                        return options.map((String value) {
+                          return Text(
+                            dropdownValue,
+                            style: const TextStyle(color: Colors.black),
+                          );
+                        }).toList();
+                        },
+                      items: options.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
               Container(
-                margin: new EdgeInsets.symmetric(vertical: 40.0),
+                margin: new EdgeInsets.symmetric(horizontal: 5, vertical: 10.0),
                 child:Row(
                   children:[
                     Text('Terrible'),
@@ -58,6 +111,28 @@ class _NewDrinkState extends State<NewDrink> {
                   ],
                 ),
               ),
+
+            Container(
+                margin: new EdgeInsets.symmetric(vertical: 60.0),
+                width:150,
+                height: 40,
+                child:ElevatedButton(
+                    onPressed: (){
+                      //todo
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent
+                    ),
+                    child: Text(
+                      'Insert Beer',
+                      style:
+                      TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    )
+                )
+            ),
 
           ],
         ),
